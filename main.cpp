4d4b0ddb645a8ad675a52ec3eb196aafffe77bec
Yash2402 <mrsmeenakumaris@gmail.com>
2024-07-21 11:04:12 +0530

    added multiple particles 	modified:   main.cpp 	modified:   particle.h

--- main.cpp
+++ main.cpp
@@ -1,23 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <stdlib.h>
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
 #include <SDL2/SDL_timer.h>
 #include <SDL2/SDL2_gfxPrimitives.h>
 #include "particle.h"
-#define SDL_HINT_RENDER_VSYNC 1             
+#define SDL_HINT_RENDER_VSYNC "SDL_RENDER_VSYNC"             
+#define WIDTH 1200
+#define HEIGHT 900
+#define OFFSET 10
 
 int main(int argc, char *argv[])
 {
-    SDL_Window* window = nullptr;
-    SDL_Renderer* renderer = nullptr;
+    SDL_Window* window = SDL_CreateWindow("Particle System", 0, 0, WIDTH, HEIGHT, SDL_WINDOW_ALLOW_HIGHDPI);
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_PRESENTVSYNC);
     SDL_Event e;
 
     SDL_Init(SDL_INIT_EVERYTHING);
     bool running = true;
-    SDL_Rect r{10, 10, 250, 250};
-    Particle particle(320, 240, 2, 0, 0, 10, 20);
+    //  SDL_Rect r{10, 10, 250, 250};
 
-    SDL_CreateWindowAndRenderer(640, 480, 0, &window, &renderer);
-    SDL_Surface* ball = IMG_Load("./bitmap.png");
+    Particle particle1(320, 240, 10, 0, 10);
+    Particle particle2(50, 240, 4, 0, 10);
+
+    std::vector<Particle> particles;
+    for (int i = 0; i < 100; i++){
+        particles.emplace_back(rand()%WIDTH, rand()%HEIGHT, rand()%10, rand()%10, 10);
+    }
+
+    //  SDL_Surface* ball = IMG_Load("./bitmap.png");
     while(running){
         while(SDL_PollEvent(&e))
         { 
@@ -27,10 +39,12 @@ int main(int argc, char *argv[])
         SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
 
         SDL_RenderClear(renderer);
-        particle.show(renderer);
-        particle.updateParticle(1);
-        particle.edge(640, 480, 40);
-
+        for(int i = 0; i < 100; i++){
+            particles[i].show(renderer);
+            particles[i].applyForce(0, rand()%5);
+            particles[i].update();
+            particles[i].edge(WIDTH, HEIGHT, OFFSET);
+        }
         SDL_RenderPresent(renderer);
         SDL_Delay(10);
     }
--- particle.h
+++ particle.h
@@ -3,18 +3,25 @@
 class Particle{
     public:
         int x, y;
+        int velx;
+        int vely;
+        int accx;
+        int accy;
         int radius;
-        int velx, vely;
-        int accx, accy;
+        int mass = 1;
 
-        Particle(int x, int y, int velx, int vely, int accx, int accy, int radius): x(x), y(y), velx(velx), vely(vely), accx(accx), accy(accy), radius(radius){}
+        Particle(int x, int y, int velx, int vely, int radius): x(x), y(y), velx(velx), vely(vely), radius(radius){}
 
-        void updateParticle(int deltatime){
-            x += velx*deltatime;
-            y += vely*deltatime;
-            velx += accx*deltatime*deltatime;
-            vely += accy*deltatime*deltatime;
+        void update(){
+            x += velx;
+            y += vely;
+            velx += accx;
+            vely += accy;
             accx = 0; accy = 0;
+        }
+        void applyForce(int forcex, int forcey){
+            this->accx = (int)forcex/mass;
+            this->accy = (int)forcey/mass;
         }
 
         void show(SDL_Renderer *renderer){
@@ -22,10 +29,10 @@ class Particle{
             filledCircleRGBA(renderer, x, y, radius, 0, 0, 0, 255);
         }
         void edge(int width, int height, int offset){
-            if (this->x < offset + this->radius){ this->velx *= -1;}
-            if (this->x > width - offset - this->radius){ this->velx *= -1;}
-            if (this->y < offset + this->radius){ this->vely *= -1;}
-            if (this->y > height - offset - this->radius){ this->vely *= -1;}
+            if (this->x < offset + this->radius){ this->x = offset + this->radius; this->velx *= -0.9;}
+            if (this->x > width - offset - this->radius){ this->x = width - offset - this->radius; this->velx *= -0.9;}
+            if (this->y < offset + this->radius){ this->y = offset + this->radius; this->vely *= -0.9;}
+            if (this->y > height - offset - this->radius){ this->y = height - offset - this->radius; this->vely *= -0.9;}
         }
         ~Particle(){};
 };
